1ef9b854b34458483725ebc1956195ec854272dc
masoodroghani <120813471+masoodroghani@users.noreply.github.com>
2026-09-18 22:25:08 +0500

    Update build.gradle.kts

--- LocationSecurityLab/AppA-LocationSimulator/app/build.gradle.kts
+++ LocationSecurityLab/AppA-LocationSimulator/app/build.gradle.kts
@@ -3,7 +3,23 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
     namespace = "com.example.locationsimulator"
     compileSdk = 35
-    defaultConfig { applicationId = "com.example.locationsimulator"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+
+    defaultConfig {
+        applicationId = "com.example.locationsimulator"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
